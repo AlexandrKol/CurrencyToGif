289405a014dabb652b705f31af197100d100940f
AlexandrKol <45937644+AlexandrKol@users.noreply.github.com>
2021-10-13 14:10:40 +0300

    Update Main.java

--- src/main/java/com/example/currency_to_gif/Main.java
+++ src/main/java/com/example/currency_to_gif/Main.java
@@ -1,1 +1,17 @@
+package com.example.currency_to_gif;
 
+/**
+ *
+ * @author A.Kolesov
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new CurrencyController();
+        
+    }
+    
+}
