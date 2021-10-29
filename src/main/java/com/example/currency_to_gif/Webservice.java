54704fee3e192b6ce78077c29b2ca5f9e7026710
AlexandrKol <45937644+AlexandrKol@users.noreply.github.com>
2021-10-29 19:08:37 +0300

    Update Webservice.java

--- src/main/java/com/example/currency_to_gif/Webservice.java
+++ src/main/java/com/example/currency_to_gif/Webservice.java
@@ -15,19 +15,27 @@ import feign.RequestLine;
  * @author A.Kolesov
  */
 interface Webservice {
-    @RequestLine("GET /v1/gifs/search?api_key=fZ55Teqzm9ydbO7HzUqwu1WFlR9uQ34m&q=rich&limit=1&offset={offset}")
+    
+    // https://api.giphy.com
+    String  Gif_api_key ="fZ55Teqzm9ydbO7HzUqwu1WFlR9uQ34m";
+    
+    // https://openexchangerates.org
+    String  OpenExchangeRates_api_key ="e5c0d8d370794826bface29b26a67634";
+    
+    
+    @RequestLine("GET /v1/gifs/search?api_key="+Gif_api_key+"&q=rich&limit=1&offset={offset}")
 @Headers("Content-Type: application/json")
  JsonObject      rich (@Param("offset") int offset);
    
- @RequestLine("GET /v1/gifs/search?api_key=fZ55Teqzm9ydbO7HzUqwu1WFlR9uQ34m&q=broke&limit=1&offset={offset}")
+ @RequestLine("GET /v1/gifs/search?api_key="+Gif_api_key+"&q=broke&limit=1&offset={offset}")
 @Headers("Content-Type: application/json")
  JsonObject      broke (@Param("offset") int offset);   
  
-  @RequestLine("GET /api/latest.json?app_id=e5c0d8d370794826bface29b26a67634")
+  @RequestLine("GET /api/latest.json?app_id="+ OpenExchangeRates_api_key)
 @Headers("Content-Type: application/json")
  JsonObject     getCurrentDay ();  
  
-   @RequestLine("GET /api/historical/{day}?app_id=e5c0d8d370794826bface29b26a67634")
+   @RequestLine("GET /api/historical/{day}?app_id=" + OpenExchangeRates_api_key)
 @Headers("Content-Type: application/json")
  JsonObject     getCurrentOld (@Param("day") String day);
-}
+}}
